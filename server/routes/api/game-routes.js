51bc481ef08d95b0d64e2dc4503f315459985960
smardone <shaun.mardones@gmail.com>
2021-03-14 22:45:46 -0400

    created main game route

---
+++ server/routes/api/game-routes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const { Game } = require('../../models/game');
+
+router.get('/', (req, res) => {
+    
+    Game.findAll()
+        .then(dbGameData => res.json(dbGameData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
